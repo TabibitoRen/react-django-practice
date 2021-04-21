1f5f33e4c23a5a6823357dfa5a5a5b3b2ed4b0be
Ren <nakurakanra@hotmail.com>
2021-04-21 10:44:02 +0100

    register token now works

--- leadmanager/leads/api.py
+++ leadmanager/leads/api.py
@@ -8,8 +8,9 @@ class LeadViewSet(viewsets.ModelViewSet):
         permissions.IsAuthenticated
     ]
     serializer_class = LeadSerializer
-def get_queryset(self):
-    return self.request.user.leads.all()
+    
+    def get_queryset(self):
+        return self.request.user.leads.all()
 
-def perform_create(self,serializer):
-    serializer.save(owner=self.request.user)
+    def perform_create(self,serializer):
+        serializer.save(owner=self.request.user)
