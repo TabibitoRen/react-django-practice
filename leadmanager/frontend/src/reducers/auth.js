4cb59424857c1d061fcedcdd35eb3267c1548aa5
Ren <nakurakanra@hotmail.com>
2021-04-21 16:00:39 +0100

    ability to login added

--- leadmanager/frontend/src/reducers/auth.js
+++ leadmanager/frontend/src/reducers/auth.js
@@ -1,4 +1,10 @@
-import { USER_LOADED, USER_LOADING, AUTH_ERROR } from '../actions/types';
+import {
+  USER_LOADED,
+  USER_LOADING,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+} from '../actions/types';
 
 const initialState = {
   token: localStorage.getItem('token'),
@@ -21,7 +27,24 @@ export default function (state = initialState, action) {
         isLoading: false,
         user: action.payload,
       };
+    case LOGIN_SUCCESS:
+      localStorage.setItem('token', action.payload.token);
+      return {
+        ...state,
+        ...action.payload,
+        isAuthenticated: true,
+        isLoading: false,
+      };
     case AUTH_ERROR:
+      localStorage.removeItem('token');
+      return {
+        ...state,
+        token: null,
+        user: null,
+        isAuthenticated: false,
+        isLoading: false,
+      };
+    case LOGIN_FAIL:
       localStorage.removeItem('token');
       return {
         ...state,
