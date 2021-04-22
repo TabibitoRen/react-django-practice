9fa2b0becd9dff75ed116efbaac82e7353f66d5d
Ren <nakurakanra@hotmail.com>
2021-04-22 11:19:42 +0100

    can now create leads tied to an account

--- leadmanager/frontend/src/components/layout/Header.js
+++ leadmanager/frontend/src/components/layout/Header.js
@@ -14,6 +14,9 @@ export class Header extends Component {
 
     const authLinks = (
       <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
+        <span className='navbar-text mr-3'>
+          <strong>{user ? `Welcome ${user.username}` : ''}</strong>
+        </span>
         <li className='nav-item'>
           <button
             onClick={this.props.logout}
